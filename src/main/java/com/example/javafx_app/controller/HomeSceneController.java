69f65d360a81d0121d35b61692b060ef5941e6e4
biasop <lebatiem012@gmail.com>
2025-11-19 16:31:24 +0700

    thêm scene account và scene lịch sử giao dịch

--- src/main/java/com/example/javafx_app/controller/HomeSceneController.java
+++ src/main/java/com/example/javafx_app/controller/HomeSceneController.java
@@ -70,7 +70,7 @@ public class HomeSceneController {
 
     @FXML
     void XemDIchVu(ActionEvent event) {
-
+        SceneUtils.switchScene(SceneUtils.getStageFromEvent(event), "ServiceScene/service_home_scene.fxml");
     }
 
     @FXML
@@ -85,12 +85,12 @@ public class HomeSceneController {
 
     @FXML
     void XemLichSuGiaoDich(ActionEvent event) {
-
+        SceneUtils.switchScene(SceneUtils.getStageFromEvent(event), "TransactionScene/transaction_history_scene.fxml");
     }
 
     @FXML
     void XemTaiKhoan(ActionEvent event) {
-
+        SceneUtils.switchScene(SceneUtils.getStageFromEvent(event), "/com/example/javafx_app/account_scene.fxml");
     }
 
     @FXML
